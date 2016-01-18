d305f4475458a608233d4a1510e30c9235ae4c8a
Vinod Vishwakarma <vinod@vinods-mac-mini.local>
2016-01-18 15:46:21 +0530

    Initial Commit

---
+++ SwiftCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCamera
+//
+//  Created by Vinod Vishwakarma on 18/01/16.
+//  Copyright © 2016 Vinod Vishwakarma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
