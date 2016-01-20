4ea2455147f8a734be2dd0ca7fa614bfe3a7f7be
Vinod Vishwakarma <vinod@Vinods-Mac-mini.local>
2016-01-20 15:18:32 +0530

    first commit

---
+++ SwiftCamera/SwiftCamera-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SwiftCamera-Bridging-Header.h
+//  SwiftCamera
+//
+//  Created by Vinod Vishwakarma on 18/01/16.
+//  Copyright © 2016 Vinod Vishwakarma. All rights reserved.
+//
+
+#ifndef SwiftCamera_Bridging_Header_h
+#define SwiftCamera_Bridging_Header_h
+
+
+#endif /* SwiftCamera_Bridging_Header_h */
